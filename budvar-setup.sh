ab3f79a10262635da17a00206f93358df5b5a9e0
Paul Ostazeski <paul.ostazeski@gmail.com>
2015-04-14 16:03:41 -0400

    Update budvar-setup.sh

--- budvar-setup.sh
+++ budvar-setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -ex
-yes | sudo pacman -S zsh tmux 
+sudo pacman --noconfirm -S zsh tmux 
 sudo sed -i "/postazeski/ s/bash/zsh/" /etc/passwd 
 (
   set +e
-  cd ~/.dotfiles/fasd && yes | makepkg -fsicC
+  cd ~/.dotfiles/fasd && makepkg --noconfirm -fsicC
 )
-exit
-
